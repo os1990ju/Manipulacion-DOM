46df0af826f0ab87a26b4c6a73b6f54b8b97de90
Oscar Jurado <os1990ju@gmail.com>
2022-10-22 22:14:55 -0500

    finalizando create task

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const btn = document.querySelector('[data-form-btn]'); //data atributes
 const createTask = (e)=>{
     e.preventDefault();
-    const input = document.querySelector('[data-input-task]')
-    console.log(input.value);
+    const input = document.querySelector('[data-input-task]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]')
+    const task = document.createElement('li');
+    task.classList.add('card');
     input.value='';
+    const content = `<div><i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span></div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+        task.innerHTML = content
+    list.appendChild(task)
 }
 
 btn.addEventListener('click', createTask)
